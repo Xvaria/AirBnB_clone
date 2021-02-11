5b81820678a40855a9f71fb085caa8efceafce4a
Nicolas <nico15935746@gmail.com>
2021-02-10 20:55:37 -0500

    Just kill the __

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from time import sleep
 
 
 class BaseModel:
-    """  """
-    def __init__(self, id=None, updated_at=None, created_at=None):
+    def __init__(self, id=None, created_at=datetime.now(), updated_at=None):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = created_at
+        self.updated_at = BaseModel.save(self)
 
     @property
     def updated_at(self):
-        return self.updated_at
+        return(self.updated_at)
 
     @updated_at.setter
-    def updated_at(self, berga):
-        self.updated_at = berga
+    def updated_at(self, time=datetime.now()):
+        self.__updated_at = time
+
+    def save(self):
+        return(datetime.now())
+
+    def __str__(self):
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
 
 
 a = BaseModel()
-print(a.created_at)
+print(a)
