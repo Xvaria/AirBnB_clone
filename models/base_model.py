29ad754192710e580c7e5d6e59b8d48295940b26
Nicolas <nico15935746@gmail.com>
2021-02-11 18:49:50 -0500

    pgsql is being a pain

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,17 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],\
+ '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],\
+ '%Y-%m-%dT%H:%M:%S.%f')
+            self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__,
