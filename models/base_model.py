12406276acb62ef83e5d98db61523e288359b511
Xvaria <2349@holbertonschool.com>
2021-02-11 16:00:16 +0000

    Correction v2.02 dictionary updates

--- models/base_model.py
+++ models/base_model.py
@@ -4,26 +4,20 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, id=None, created_at=datetime.now(), updated_at=None):
+    def __init__(self, id=None, created_at=None, updated_at=None):
         self.id = str(uuid.uuid4())
-        self.created_at = created_at
-        self.updated_at = BaseModel.save(self)
-
-    @property
-    def updated_at(self):
-        return(self.updated_at)
-
-    @updated_at.setter
-    def updated_at(self, time=datetime.now()):
-        self.__updated_at = time
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__))
 
     def save(self):
-        return(datetime.now())
+        self.updated_at = datetime.now()
 
-
-a = BaseModel()
-print(a)
+    def to_dict(self):
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        return(self.__dict__)
