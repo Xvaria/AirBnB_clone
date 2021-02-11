7d77d376965e7451b7c0e573c0dee13868d357b2
Xvaria <2349@holbertonschool.com>
2021-02-11 00:49:41 +0000

    Correction v2.0 update method __str__

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from time import sleep
 
 
 class BaseModel:
-    """  """
-    def __init__(self, id=None, updated_at=None, created_at=None):
+    def __init__(self, id=None, created_at=datetime.now(), updated_at=None):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     @property
     def updated_at(self):
-        return self.updated_at
+        return(self.__updated_at)
 
     @updated_at.setter
-    def updated_at(self, berga):
-        self.updated_at = berga
+    def updated_at(self, time=datetime.now()):
+        self.__updated_at = time
 
+    def __str__(self):
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
 
 a = BaseModel()
-print(a.created_at)
+print(a)
