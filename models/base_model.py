5cd5b248f7cca2f7af2d2e40c723683b404f5907
Xvaria <2349@holbertonschool.com>
2021-02-11 01:30:47 +0000

    Correction v2.01 Update method save

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ class BaseModel:
     def __init__(self, id=None, created_at=datetime.now(), updated_at=None):
         self.id = str(uuid.uuid4())
         self.created_at = created_at
-        self.updated_at = updated_at
+        self.updated_at = BaseModel.save(self)
 
     @property
     def updated_at(self):
@@ -20,6 +20,8 @@ class BaseModel:
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__))
+    def save(self):
+        return(datetime.now())
 
 a = BaseModel()
 print(a)
