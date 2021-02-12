dcd355af5cab280aa77f4004f4a7deb62b68214a
Xvaria <2349@holbertonschool.com>
2021-02-12 21:24:30 +0000

    Correction v2.03 add methods of FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],\
- '%Y-%m-%dT%H:%M:%S.%f')
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],\
- '%Y-%m-%dT%H:%M:%S.%f')
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
             self.__dict__.update(kwargs)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__,
@@ -22,6 +24,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         self.__dict__["__class__"] = self.__class__.__name__
