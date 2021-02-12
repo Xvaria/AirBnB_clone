bb4469f784bb090788456a9b3387104406d1ad91
Xvaria <2349@holbertonschool.com>
2021-02-12 23:25:21 +0000

    Correction v2.0 update reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,24 +8,26 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        return(self.__dict__)
+        return(FileStorage.__objects)
 
     def new(self, obj):
         FileStorage.__objects[obj.__class__.__name__+ "." + obj.id] = obj
 
     def save(self):
         try:
+            sdict = {}
+            for k, v in FileStorage.__objects.items():
+                sdict[k] = v.to_dict()
             with open(self.__file_path, mode="w") as f:
-                sdict = {}
-                for k, v in self.__objects.items():
-                    sdict[k] = v.to_dict()
                 json.dump(sdict, f)
         except:
             pass
 
     def reload(self):
         try:
-            with open(self.__file_path) as f:
+            with open(self.__file_path, mode="r") as f:
                 FileStorage.__objects = json.load(f)
+            for val in FileStorage.__objects.values():
+                self.new(BaseModel(**val))
         except:
             pass
