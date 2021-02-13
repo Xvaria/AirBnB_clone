e9c99ecaf5f0f1a8dd09b37dcf2d779872da4df1
Nicolas <nico15935746@gmail.com>
2021-02-12 19:02:26 -0500

    Basic shell 0.1

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    def emptyline(self):
+        pass
     def do_EOF(self, line):
         return True
     def do_quit(self, line):
