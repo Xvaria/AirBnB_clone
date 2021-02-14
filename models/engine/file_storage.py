5ea517b03c1577a0ffc7e76483dd676bb40a4feb
Nicolas <nico15935746@gmail.com>
2021-02-14 16:44:05 -0500

    Comments for everybodyyy

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
+""" Save the file, import objects, etc """
 from models.base_model import BaseModel
 import json
 
+
 class FileStorage:
-    ''
+    """ Class file storage with methods to load, create, return, etc. """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """ Return all the objects """
         return(FileStorage.__objects)
 
     def new(self, obj):
-        FileStorage.__objects[obj.__class__.__name__+ "." + obj.id] = obj
+        """ Create the new objects """
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
-        try:
-            sdict = {}
-            for k, v in FileStorage.__objects.items():
-                sdict[k] = v.to_dict()
-            with open(self.__file_path, mode="w") as f:
-                json.dump(sdict, f)
-        except:
-            pass
+        """ Save the objects into a file """
+        sdict = {}
+        for k, v in FileStorage.__objects.items():
+            sdict[k] = v.to_dict()
+        with open(self.__file_path, mode="w") as f:
+            json.dump(sdict, f)
 
     def reload(self):
+        """ Reload a file to load objs """
         try:
             with open(self.__file_path, mode="r") as f:
                 FileStorage.__objects = json.load(f)
             for val in FileStorage.__objects.values():
                 self.new(BaseModel(**val))
-        except:
+        except FileNotFoundError:
             pass
