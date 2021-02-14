82315734857788d7399936fec877a36cee89bcc2
Nicolas <nico15935746@gmail.com>
2021-02-14 16:31:47 -0500

    Comments free

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" State of something """
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    """ Public class attribute State of something """
     name = ""
