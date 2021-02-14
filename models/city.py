8ed83eddba808a67e056f921005d674d4e140cf6
Nicolas <nico15935746@gmail.com>
2021-02-14 16:24:51 -0500

    We are happy with comments

--- models/city.py
+++ models/city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" The city class for OBJ """
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
+    """ Public class attributes state_id and name """
     state_id = ""
     name = ""
