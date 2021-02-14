24a1b187fd8fd679ffdbbfe66aef80dc22395a1b
Nicolas <nico15935746@gmail.com>
2021-02-14 16:29:18 -0500

    #Comments

--- models/review.py
+++ models/review.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" Actually i dont know """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """ More public class attributes """
     place_id = ""
     user_id = ""
     text = ""
