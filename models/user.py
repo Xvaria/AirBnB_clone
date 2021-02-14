d14de1760007b6666c9997ec60ce655fbbd18f99
Nicolas <nico15935746@gmail.com>
2021-02-14 16:35:24 -0500

    Plot twist, we all are comments

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" User data """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """ Public class attributes about user data """
     email = ""
     password = ""
     first_name = ""
