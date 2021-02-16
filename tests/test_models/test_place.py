63f0d6b1c742612eb8faa8d96231fb719958cb34
Nicolasopf <69660332+Nicolasopf@users.noreply.github.com>
2021-02-16 16:54:59 -0500

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -11,6 +11,7 @@ from models.review import Review
 
 
 class Testpep8(unittest.TestCase):
+    ''' Test for pep8 '''
     def test_pep8_conformance_place(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
@@ -18,9 +19,11 @@ class Testpep8(unittest.TestCase):
         self.assertEqual(result.total_errors, 0, "Found style errors")
 
     def test_class(self):
+        ''' Test class place '''
         place1 = Place()
         self.assertEqual(place1.__class__.__name__, "Place")
 
     def test_father(self):
+        ''' Test inherit '''
         place1 = Place()
         self.assertTrue(issubclass(place1.__class__, BaseModel))
